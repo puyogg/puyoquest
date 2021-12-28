e67e986036d51570744e3209a773b95970cb036e
Phong Tran <s2lsoftener@gmail.com>
2021-12-27 22:43:47 -0500

    Commenting some stuff out for now

--- packages/database/src/tables/cards/index.ts
+++ packages/database/src/tables/cards/index.ts
@@ -1,5 +1,6 @@
 import type { Card } from './create';
 import { create } from './create';
-import { listCharacterCards } from './list-character-cards';
+// import { listCharacterCards } from './list-character-cards';
 
-export { Card, create, listCharacterCards };
+// export { Card, create, listCharacterCards };
+export { Card, create };
