72cb759a2df9027658eb9a74162e5375f5acbb06
s2lsoftener <s2@puyonexus.com>
2022-01-30 18:01:59 -0500

    Find id of command for permission assignment

--- packages/discordbot/src/permissions.ts
+++ packages/discordbot/src/permissions.ts
@@ -1,9 +1,16 @@
 import { Client, GuildApplicationCommandPermissionData } from 'discord.js';
 
 export async function setCommandPermissions(client: Client): Promise<void> {
+  if (!client.application?.owner) await client.application?.fetch();
+  const commands = await client.guilds.cache.get('133012933260214272')?.commands.fetch();
+
+  if (!commands) return;
+  const roleMenuCommand = commands.find((command) => command.name === 'rolemenu');
+  if (!roleMenuCommand) return;
+
   const permissions: GuildApplicationCommandPermissionData[] = [
     {
-      id: '934932086832369674',
+      id: roleMenuCommand.id,
       permissions: [
         {
           id: '135988596661288961',
