d2c7825c2aaeab2777a5769881d0722c69003439
s2lsoftener <s2@puyonexus.com>
2022-09-06 09:30:32 -0400

    ppqevents command

--- packages/discordbot/src/commands/ppqevents.ts
+++ packages/discordbot/src/commands/ppqevents.ts
@@ -1,7 +1,24 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CacheType, CommandInteraction } from 'discord.js';
-import { Command } from '../types';
 import * as Facade from '@ppq-wiki/facade';
+import { CacheType, CommandInteraction, MessageEmbed } from 'discord.js';
+import { DateTime } from 'luxon';
+import { Command } from '../types';
+
+function showRemaining(end: DateTime): string {
+  const diff = end.diffNow(['years', 'months', 'days', 'hours']);
+  let days = 0;
+  let hours = 0;
+  if (diff.years > 1) {
+    return `9999 years`;
+  } else if (diff.hours < 0) {
+    days = Math.floor(diff.days + diff.hours / 24);
+    hours = Math.floor(24 + diff.hours);
+  } else {
+    days = Math.floor(diff.days);
+    hours = Math.floor(diff.hours);
+  }
+  return `${days}d ${hours}h`;
+}
 
 export const PpqEvents: Command = {
   data: new SlashCommandBuilder()
@@ -9,9 +26,63 @@ export const PpqEvents: Command = {
     .setDescription('Get the latest timed events in Puyo Quest'),
   cooldown: 1,
   async execute(interaction: CommandInteraction<CacheType>) {
-    // console.log('Events');
-    // const events = await Facade.Wiki.getTimedEvents();
+    const timedEvents = await Facade.Wiki.getTimedEvents();
 
-    return interaction.reply('Sorry, not implemented yet!');
+    const now = DateTime.now().setZone('Asia/Tokyo');
+
+    const ongoingFormatted = timedEvents.ongoingEvents.map((event) => {
+      const endTime = DateTime.fromJSDate(event.endTime, { zone: 'Asia/Tokyo' });
+      const diffHours = endTime.diff(now, 'hours').toObject()['hours'];
+      if (!diffHours) return;
+      return `•${diffHours < 24 ? '🚨' : ''}**${event.name} (${
+        event.jpname
+      })**: Ends in ${showRemaining(endTime)}\n`;
+    });
+
+    const upcomingFormatted = timedEvents.upcomingEvents.map((event) => {
+      const startTime = DateTime.fromJSDate(event.startTime, { zone: 'Asia/Tokyo' });
+      const diffHours = startTime.diff(now, 'hours').toObject()['hours'];
+      if (!diffHours) return;
+      return `•${diffHours < 24 ? '🔔' : ''}**${event.name} (${
+        event.jpname
+      })**: Starts in ${showRemaining(startTime)}\n`;
+    });
+
+    const embed = new MessageEmbed();
+    embed.setTitle(`Timed events for ${now.monthLong} ${now.year}`);
+
+    embed.setDescription(
+      `This list was generated at: ${now.year}/${now.month}/${now.day} ${now.hour}:${now.minute} JST`,
+    );
+
+    let ongoingText = '';
+    let ongoingTitle = 'Ongoing Events';
+    for (const line of ongoingFormatted) {
+      if ((ongoingText + line).length > 1024) {
+        embed.addField(ongoingTitle, ongoingText);
+        ongoingText = '';
+        ongoingTitle = 'Ongoing Events (cont.)';
+      }
+      ongoingText += line;
+    }
+    if (ongoingText) embed.addField(ongoingTitle, ongoingText);
+
+    let upcomingText = '';
+    let upcomingTitle = 'Upcoming Events';
+    for (const line of upcomingFormatted) {
+      if ((upcomingText + line).length > 1024) {
+        embed.addField(upcomingTitle, upcomingText);
+        upcomingText = '';
+        upcomingTitle = 'Upcoming Events (cont.)';
+      }
+      upcomingText += line;
+    }
+    if (upcomingText) embed.addField(upcomingTitle, upcomingText);
+
+    const url = encodeURI(
+      `https://puyonexus.com/wiki/PPQ:Event_News_Archive/${now.monthLong}_${now.year}`,
+    );
+    embed.setURL(url);
+    return interaction.reply({ embeds: [embed] });
   },
 };
--- packages/facade/src/entities/wiki/timed-events.ts
+++ packages/facade/src/entities/wiki/timed-events.ts
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import { Util } from '../..';
 
 export interface EventData {
@@ -11,7 +11,54 @@ export interface EventData {
   feature: string;
 }
 
-export async function getTimedEvents(): Promise<void> {
+export interface EventDataWithDates extends EventData {
+  startTime: Date;
+  endTime: Date;
+}
+
+function parseTime(timeStr: string, isStartTime: boolean): DateTime {
+  if (!timeStr) {
+    const time = DateTime.fromJSDate(new Date(), { zone: 'Asia/Tokyo' });
+    const duration = Duration.fromObject({
+      years: 24,
+    });
+
+    return time.plus(duration);
+  }
+  const [year, month, day] = timeStr
+    .split(' ')[0]
+    .split('/')
+    .map((num) => parseInt(num, 10));
+
+  // TODO refactor these annoying ternaries too
+  const [hour, minute] =
+    timeStr.split(' ').length > 1
+      ? timeStr
+          .split(' ')[1]
+          .split(':')
+          .map((num) => parseInt(num, 10))
+      : isStartTime
+      ? [15, 0]
+      : [23, 59];
+
+  const time = DateTime.fromObject(
+    {
+      year: year,
+      month: month,
+      day: day,
+      hour: hour,
+      minute: minute,
+    },
+    { zone: 'Asia/Tokyo' },
+  );
+
+  return time;
+}
+
+export async function getTimedEvents(): Promise<{
+  ongoingEvents: EventDataWithDates[];
+  upcomingEvents: EventDataWithDates[];
+}> {
   const time = DateTime.fromJSDate(new Date(), { zone: 'Asia/Tokyo' });
 
   const res = await Axios.get<string>(
@@ -27,5 +74,84 @@ export async function getTimedEvents(): Promise<void> {
 
   const events = eventTemplateText.map((event) => Util.parseTemplate<EventData>(event));
 
-  console.log(events);
+  // Filter out events that had already passed.
+  const validEvents = events
+    .filter((event) => {
+      // Edge case: event is still in preview
+      if (!event.end) {
+        const [yearEnd, monthEnd, dayEnd] = [9999, 9, 9];
+        const [hourEnd, minuteEnd] = [9, 9];
+        const endTime = DateTime.fromObject(
+          {
+            year: yearEnd,
+            month: monthEnd,
+            day: dayEnd,
+            hour: hourEnd,
+            minute: minuteEnd,
+          },
+          { zone: 'Asia/Tokyo' },
+        );
+
+        return endTime > time;
+      }
+
+      // TODO: refactor these parts into separate functions that can
+      // be unit tested.
+      const [yearEnd, monthEnd, dayEnd] = event.end
+        .split(' ')[0]
+        .split('/')
+        .map((num) => parseInt(num, 10));
+      const [hourEnd, minuteEnd] =
+        event.end.split(' ').length > 1
+          ? event.end
+              .split(' ')[1]
+              .split(':')
+              .map((num) => parseInt(num, 10))
+          : [23, 59];
+
+      const endTime = DateTime.fromObject(
+        {
+          year: yearEnd,
+          month: monthEnd,
+          day: dayEnd,
+          hour: hourEnd,
+          minute: minuteEnd,
+        },
+        { zone: 'Asia/Tokyo' },
+      );
+
+      return endTime > time;
+    })
+    .map((event) => {
+      const startTime = parseTime(event.start, true).toJSDate();
+      const endTime = parseTime(event.end, false).toJSDate();
+      const name = event.name.replaceAll(/<br\s*?\/>/g, ' ');
+      return {
+        ...event,
+        startTime,
+        endTime,
+        name,
+      };
+    });
+
+  const ongoingEvents = validEvents.filter((event) => {
+    const date = DateTime.fromJSDate(event.startTime, { zone: 'Asia/Tokyo' });
+    return date <= time;
+  });
+
+  const upcomingEvents = validEvents.filter((event) => {
+    const date = DateTime.fromJSDate(event.startTime, { zone: 'Asia/Tokyo' });
+    return date > time;
+  });
+
+  return { ongoingEvents, upcomingEvents };
 }
+
+(async () => {
+  if (require.main !== module) {
+    return;
+  }
+
+  const result = await getTimedEvents();
+  console.log(result);
+})();
