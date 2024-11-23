18a7c99b09b235557a9fdff4d8874cb1a00e74e3
Phong Tran <s2lsoftener@gmail.com>
2024-11-23 13:00:17 -0500

    api: refactor get card by id

--- services/api/src/cards/get_by_id.rs
+++ services/api/src/cards/get_by_id.rs
@@ -20,15 +20,8 @@ pub enum GetByIdResponse {
     NotFound(PlainText<String>),
 }
 
-pub async fn get_by_id(
-    api_config: &ApiConfig,
-    pool: &PgPool,
-    redis_client: &RedisClient,
-    wiki_client: &WikiClient,
-    s3_client: &S3BackupClient,
-    id: &str,
-) -> Result<GetByIdResponse> {
-    let card_db: Option<CardDb> = sqlx::query_as(
+pub async fn query_get_by_id(pool: &PgPool, id: &str) -> Result<Option<CardDb>, poem::Error> {
+    sqlx::query_as(
         r#"
             SELECT *
             FROM card
@@ -39,19 +32,41 @@ pub async fn get_by_id(
     .bind(&id)
     .fetch_optional(pool)
     .await
-    .map_err(InternalServerError)?;
+    .map_err(InternalServerError)
+}
 
-    match card_db {
-        Some(card_db) => {
-            let card = Card::upgrade_card_db(
-                api_config,
-                redis_client,
-                wiki_client,
-                s3_client,
-                card_db
-            ).await?;
-            Ok(GetByIdResponse::Card(Json(card)))
-        },
+pub async fn fetch_card_by_id(
+    api_config: &ApiConfig,
+    pool: &PgPool,
+    redis_client: &RedisClient,
+    wiki_client: &WikiClient,
+    s3_client: &S3BackupClient,
+    id: &str,
+) -> Result<Option<Card>, poem::Error> {
+    let card_db = query_get_by_id(pool, id).await?;
+    let card = match card_db {
+        None => None,
+        Some(card_db) => Some(
+            Card::upgrade_card_db(api_config, redis_client, wiki_client, s3_client, card_db)
+                .await?,
+        ),
+    };
+
+    Ok(card)
+}
+
+pub async fn get_by_id(
+    api_config: &ApiConfig,
+    pool: &PgPool,
+    redis_client: &RedisClient,
+    wiki_client: &WikiClient,
+    s3_client: &S3BackupClient,
+    id: &str,
+) -> Result<GetByIdResponse> {
+    let card = fetch_card_by_id(api_config, pool, redis_client, wiki_client, s3_client, id).await?;
+
+    match card {
+        Some(c) => Ok(GetByIdResponse::Card(Json(c))),
         None => Ok(GetByIdResponse::NotFound(PlainText(format!(
             "Character with id {} not found",
             &id,
