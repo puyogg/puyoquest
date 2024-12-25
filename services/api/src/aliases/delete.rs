ab74cc4ac8c12f8e4fe192fc65d737e009806319
Phong Tran <s2lsoftener@gmail.com>
2024-12-25 00:29:42 -0500

    api: don't allow deletion of internal aliases

--- services/api/src/aliases/delete.rs
+++ services/api/src/aliases/delete.rs
@@ -6,6 +6,8 @@ use poem_openapi::{
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 
+use super::types::Alias;
+
 #[derive(Debug, Clone, Serialize, Deserialize, Object)]
 pub struct DeleteCount {
     count: u64,
@@ -18,6 +20,9 @@ pub enum AliasDeleteResponse {
 
     #[oai(status = 400)]
     InvalidName(PlainText<String>),
+
+    #[oai(status = 403)]
+    InternalAlias(PlainText<String>),
 }
 
 pub async fn delete(pool: &PgPool, name: &str) -> poem::Result<AliasDeleteResponse> {
@@ -28,6 +33,35 @@ pub async fn delete(pool: &PgPool, name: &str) -> poem::Result<AliasDeleteRespon
             "Invalid alias".to_string(),
         )));
     }
+
+    let current_alias = sqlx::query_as::<_, Alias>(
+        r#"
+            SELECT *
+            FROM alias
+            WHERE alias = $1
+            LIMIT 1
+        "#,
+    )
+    .bind(&normalized_alias)
+    .fetch_optional(pool)
+    .await
+    .map_err(InternalServerError)?;
+
+    match current_alias {
+        Some(current) => {
+            if current.internal == true {
+                return Ok(AliasDeleteResponse::InternalAlias(PlainText(format!(
+                    "{} is an internal alias and can't be deleted.",
+                    current.alias
+                ))));
+            }
+        }
+        None => {
+            return Ok(AliasDeleteResponse::SuccessfullyDeleted(Json(
+                DeleteCount { count: 0 },
+            )))
+        }
+    };
 
     let delete_count = sqlx::query(
         r#"
